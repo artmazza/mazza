c923a9042b42d731579514ab6080f4bc12b6acfe
artmazza <art.mazza@gmail.com>
2019-05-13 11:22:26 +1000

    added to my github

--- Test_file.java
+++ Test_file.java
@@ -1,6 +1,18 @@
-public class Test{
+import java.util.Scanner;
+
+public class Test_file{
   public static void main(String[] args){
-    int a = 2;
-    char b = 'b';
+    	Scanner input = new Scanner(System.in);
+	
+	int num = 0;
+	System.out.println("Please enter an array length");
+        num = input.nextInt();
+	int arr[] = new int[num];
+	
+	for(int i=0; i<arr.length; i++){
+		System.out.println("Enter numbers to add to array");
+		arr[i] = input.nextInt();
+	}
+	
   }
 }
